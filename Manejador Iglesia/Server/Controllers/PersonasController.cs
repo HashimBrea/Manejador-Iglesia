9ac77930c11ff7734c850e5b56533f7c735aa153
Hashim <hash.101@hotmail.com>
2020-12-02 14:47:41 -0400

    Commit 1

---
+++ Manejador Iglesia/Server/Controllers/PersonasController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Manejador_Iglesia.Server.Data;
+using Manejador_Iglesia.Shared.Models;
+
+namespace Manejador_Iglesia.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Persona>>> GetPersona()
+        {
+            return await _context.Persona.ToListAsync();
+        }
+
+        // GET: api/Personas/5
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Persona>> GetPersona(int id)
+        {
+            var persona = await _context.Persona.FindAsync(id);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return persona;
+        }
+
+        // PUT: api/Personas/5
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersona(int id, Persona persona)
+        {
+            if (id != persona.PersonaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(persona).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Personas
+        
+        [HttpPost]
+        public async Task<ActionResult<Persona>> PostPersona(Persona persona)
+        {
+            _context.Persona.Add(persona);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersona", new { id = persona.PersonaId }, persona);
+        }
+
+        // DELETE: api/Personas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Persona>> DeletePersona(int id)
+        {
+            var persona = await _context.Persona.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persona.Remove(persona);
+            await _context.SaveChangesAsync();
+
+            return persona;
+        }
+
+        private bool PersonaExists(int id)
+        {
+            return _context.Persona.Any(e => e.PersonaId == id);
+        }
+    }
+}
---
+++ Manejador Iglesia/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Manejador_Iglesia.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manejador_Iglesia.Shared.Models;
+
+namespace Manejador_Iglesia.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
